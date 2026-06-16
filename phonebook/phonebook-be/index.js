55d86bff97cc7e018618fa0e1bf0ce39c3961b43
Diana Ngo <dngo199@gmail.com>
2026-06-16 19:52:44 -0400

    uibuild

--- phonebook/phonebook-be/index.js
+++ phonebook/phonebook-be/index.js
@@ -2,13 +2,96 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
-
 app.use(express.json());
 
 morgan.token("body", (request) => {
   return JSON.stringify(request.body);
 });
 
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
-);
+app.use(morgan(":response-time ms - :method :url - status :status - :body"));
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({ error: "name is missing" });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ error: "number is missing" });
+  }
+
+  const newPerson = {
+    id: String(Math.floor(Math.random() * 100000)),
+    name: request.body.name,
+    number: request.body.number,
+  };
+
+  if (persons.some((person) => body.name === person.name)) {
+    return response
+      .status(400)
+      .json({ error: "name already exists in phonebook" });
+  }
+
+  persons.push(newPerson);
+  response.json(newPerson);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`,
+  );
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
