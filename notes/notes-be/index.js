a585bcda97e98872dc7bc4ceca86abe983b6fad5
Diana Ngo <dngo199@gmail.com>
2026-06-18 03:53:36 -0400

    uibuild

--- notes/notes-be/index.js
+++ notes/notes-be/index.js
@@ -1,94 +1,94 @@
-require("dotenv").config();
-const express = require("express");
-const Note = require("./models/note");
+require('dotenv').config()
+const express = require('express')
+const Note = require('./models/note')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(express.static("dist"));
+app.use(express.json())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
-  console.log(request.method, request.url);
-  next();
-};
+  console.log(request.method, request.url)
+  next()
+}
 
-app.use(requestLogger);
+app.use(requestLogger)
 
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => response.json(notes));
-});
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then((notes) => response.json(notes))
+})
 
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body;
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  });
+  })
 
   note
     .save()
     .then((savedNote) => response.json(savedNote))
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.get("/api/notes/:id", (request, response, next) => {
-  const id = request.params.id;
+app.get('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
 
   Note.findById(id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/notes/:id", (request, response, next) => {
-  const id = request.params.id;
+app.put('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
 
   Note.findById(id)
     .then((note) => {
       if (!note) {
-        return response.status(404).end();
+        return response.status(404).end()
       }
 
-      note.content = request.body.content;
-      note.important = request.body.important;
+      note.content = request.body.content
+      note.important = request.body.important
 
       return note.save().then((updatedNote) => {
-        response.json(updatedNote);
-      });
+        response.json(updatedNote)
+      })
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
+app.delete('/api/notes/:id', (request, response, next) => {
+  const id = request.params.id
   Note.findByIdAndDelete(id)
-    .then((result) => response.status(204).end())
-    .catch((error) => next(error));
-});
+    .then(() => response.status(204).end())
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-app.use(unknownEndpoint);
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
-app.use(errorHandler);
+  next(error)
+}
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
